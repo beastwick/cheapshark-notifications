6e066a94025f17d6268219d8263e52ddb1dd88f7
brian <brian@servitor>
2023-01-24 10:22:59 -0500

    python for sorting

--- email-best.py
+++ email-best.py
@@ -1,68 +1,110 @@
 #import pdb
 
 import json
-import pprint
+import re
 import requests
 import subprocess
+
 from operator import itemgetter
 
 def storeText(id):
-  match int(id):
-    case 1:
-      return "Steam"
-    case 2:
-      return "GamersGate"
-    case 3:
-      return "GreenManGaming"
-    case 7:
-      return "GOG"
-    case 8:
-      return "Origin"
-    case 11:
-      return "Humble Bundle"
-    case 13:
-      return "Ubisoft Store"
-    case 15:
-      return "Fanatical"
-    case 21:
-      return "WinGameStore"
-    case 23:
-      return "GameBillet"
-    case 24:
-      return "Voidu"
-    case 25:
-      return "Epic Games"
-    case 27:
-      return "Gamesplanet"
-    case 28:
-      return "Gamesload"
-    case 30:
-      return "IndieGala"
-    case 31:
-      return "Blizzard Shop"
-    case 33:
-      return "DLGamer"
-    case 34:
-      return "Noctre"
-    case 35:
-      return "DreamGame"
-    case _:
-      return "Check Cheapshark.com for originating site."
+  if id ==  1:
+    return "Steam"
+  elif id == 2:
+    return "GamersGate"
+  elif id == 3:
+    return "GreenManGaming"
+  elif id == 7:
+    return "GOG"
+  elif id == 8:
+    return "Origin"
+  elif id == 11:
+    return "Humble Bundle"
+  elif id == 13:
+    return "Ubisoft Store"
+  elif id == 15:
+    return "Fanatical"
+  elif id == 21:
+    return "WinGameStore"
+  elif id == 23:
+    return "GameBillet"
+  elif id == 24:
+    return "Voidu"
+  elif id == 25:
+    return "Epic Games"
+  elif id == 27:
+    return "Gamesplanet"
+  elif id == 28:
+    return "Gamesload"
+  elif id == 29:
+    return "2Game"
+  elif id == 30:
+    return "IndieGala"
+  elif id == 31:
+    return "Blizzard Shop"
+  elif id == 33:
+    return "DLGamer"
+  elif id == 34:
+    return "Noctre"
+  elif id == 35:
+    return "DreamGame"
+  else: 
+    return "Check Cheapshark.com for originating site."
 
 resp = requests.get(url='https://www.cheapshark.com/api/1.0/deals?&pageSize=60&pageNumber=0')
 data = resp.json()
 
 games = []
-
 for deal in data:
   if float(deal['dealRating']) >= 9.0 or (float(deal['dealRating']) >= 8.0 and int(deal['metacriticScore']) >= 90):
-    details = (deal['gameID'], deal['title'], float(deal['salePrice']), storeText(deal['storeID']), deal['metacriticScore'])
+    details = (deal['gameID'], deal['title'], float(deal['salePrice']), storeText(int(deal['storeID'])), deal['metacriticScore'])
     games.append(details)
 
 games = set(games)
 games = sorted(games, key=itemgetter(2,4))
 
+sales = []
 for game in games:
-    print('$' + format(game[2], '.2f') + ', ' + game[1] + ', ' + game[3])
+  sales.append('$' + format(game[2], '.2f') + ', ' + game[1] + ', ' + game[3] + '\n') 
 
-#subprocess.Popen['/bin/sh', '-c', 'sh email-best']
+prev = (open('prev', 'r')).readlines()
+
+# What is new.
+new = sales.copy()
+for old in prev:
+  new.remove(old)
+
+# What is the same.
+same = []
+for sale in sales:
+  if sale in prev:
+    same.append(sale)
+
+# What is gone.
+gone = prev.copy()
+if prev:
+    for sale in sales:
+      gone.remove(sale)
+
+prev =(open('prev', 'w'))
+for sale in sales:
+  prev.write(sale)
+prev.close()
+
+if new:
+  print('New:\n')
+  for entry in new:
+    print(entry, end='')
+
+if same:
+  print('\nSame:\n')
+  for entry in same:
+    print(entry, end='')
+
+if gone:
+  print('\nGone:\n')
+  for entry in gone:
+    print(entry, end='')
+
+#if new or gone:
+  #subprocess.Popen(['/usr/bin/msmtp','-a default','bostwickbrian@gmail.com', 'Subject:Cheapshark Best Deals\n\nTest'], stdout=subprocess.PIPE, universal_newlines=True) 
